17dc0049fd0431dae72c3c1deae27acbe4545b95
Vlad Romila <romilavlad2001@gmail.com>
2018-11-17 23:16:15 +0200

    Updates

---
+++ precache-manifest.5bcd2de08f70f1a54d0e595c51cd8a18.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e162bcfaa22769333027",
+    "url": "/atestat/static/js/runtime~main.e162bcfa.js"
+  },
+  {
+    "revision": "972041f2a69512cdeb1b",
+    "url": "/atestat/static/js/main.972041f2.chunk.js"
+  },
+  {
+    "revision": "289b841b76886c41bfe4",
+    "url": "/atestat/static/js/1.289b841b.chunk.js"
+  },
+  {
+    "revision": "972041f2a69512cdeb1b",
+    "url": "/atestat/static/css/main.e9c381c4.chunk.css"
+  },
+  {
+    "revision": "37a4ed0194f7f6113287b3ab2d1726ec",
+    "url": "/atestat/index.html"
+  }
+];
