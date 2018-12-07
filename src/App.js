3a8916c4a7f400276698236e9ebf7426bd7fd979
Vlad Romila <romilavlad2001@gmail.com>
2018-12-07 18:56:17 +0200

    Atestat-Romila Vlad 2020

--- src/App.js
+++ src/App.js
@@ -42,9 +42,9 @@ class App extends Component {
     </div>
   </div>
 </nav>
-          <Route path="/Atestat/" exact component={Landing} />
-          <Route path="/Atestat/pbs" exact component={List} />
-          <Route path="/Atestat/pb/:handle" exact component={PP} />
+          <Route path="/" exact component={Landing} />
+          <Route path="/pbs" exact component={List} />
+          <Route path="/pb/:handle" exact component={PP} />
           </React.Fragment>
           
       </Router>
